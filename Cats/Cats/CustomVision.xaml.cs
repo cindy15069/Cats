bc7f8dabfb8e97f5a6432df710c20a175d5e9ec7
cindy15069 <cindy15069@gmail.com>
2017-07-19 22:28:58 +1200

    Add project files.

---
+++ Cats/Cats/CustomVision.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Cats.Model;
+using Newtonsoft.Json;
+
+namespace Cats
+{
+	public partial class CustomVision : ContentPage
+	{
+		public CustomVision()
+		{
+			InitializeComponent();
+		}
+
+		private async void LoadCamera(object sender, EventArgs e)
+		{
+			await CrossMedia.Current.Initialize();
+
+			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+			{
+				await DisplayAlert("No Camera", ":( No camera available.", "OK");
+				return;
+			}
+
+			MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+			{
+				PhotoSize = PhotoSize.Medium,
+				Directory = "Sample",
+				Name = $"{DateTime.UtcNow}.jpg"
+			});
+
+			if (file == null)
+				return;
+
+			image.Source = ImageSource.FromStream(() =>
+			{
+				return file.GetStream();
+			});
+
+			await MakePredictionRequest(file);
+		}
+
+		static byte[] GetImageAsByteArray(MediaFile file)
+		{
+			var stream = file.GetStream();
+			BinaryReader binaryReader = new BinaryReader(stream);
+			return binaryReader.ReadBytes((int)stream.Length);
+		}
+
+		async Task MakePredictionRequest(MediaFile file)
+		{
+			var client = new HttpClient();
+
+			client.DefaultRequestHeaders.Add("Prediction-Key", "4edcf305771047fc8a6ebcbca72fb57d");
+
+			string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/9d5f48a5-124a-448d-a718-48b6b80de493/image?iterationId=de8fca21-bc90-4860-b74f-3b537e354464";
+
+			HttpResponseMessage response;
+
+			byte[] byteData = GetImageAsByteArray(file);
+
+			using (var content = new ByteArrayContent(byteData))
+			{
+
+				content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+				response = await client.PostAsync(url, content);
+
+
+				if (response.IsSuccessStatusCode)
+				{
+					var responseString = await response.Content.ReadAsStringAsync();
+
+					EvaluationModel responseModel = JsonConvert.DeserializeObject<EvaluationModel>(responseString);
+
+					double max = responseModel.Predictions.Max(m => m.Probability);
+
+					TagLabel.Text = (max >= 0.5) ? "Cat" : "Not Cat";
+
+				}
+
+				//Get rid of file once we have finished using it
+				file.Dispose();
+			}
+		}
+	}
+}
