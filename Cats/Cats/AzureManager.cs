b2a4b3cc2fda8b5d5859f562ef070e358a7a3f8c
cindy15069 <cindy15069@gmail.com>
2017-07-22 23:41:12 +1200

    change 10

---
+++ Cats/Cats/AzureManager.cs
@@ -0,0 +1,47 @@
+﻿using Cats.DataModels;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats
+{
+    public class AzureManager
+    {
+
+        private static AzureManager instance;
+        private MobileServiceClient client;
+
+        private IMobileServiceTable<NotCatModel> notCatTable;
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("https://myapplicationcat.azurewebsites.net/");
+            this.notCatTable = this.client.GetTable<NotCatModel>();
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+        public async Task<List<NotCatModel>> GetCatInformation()
+        {
+            return await this.notCatTable.ToListAsync();
+        }
+    }
+}
--- Cats/Cats/AzureTable.xaml.cs
+++ Cats/Cats/AzureTable.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,8 @@ namespace Cats
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AzureTable : ContentPage
 	{
-		public AzureTable()
+        MobileServiceClient client = AzureManager.AzureManagerInstance.AzureClient;
+        public AzureTable()
 		{
 			InitializeComponent();
 		}
