c4434d6e37e1f4d0c2a4bbc14a3a970cec513128
cindy15069 <cindy15069@gmail.com>
2017-07-28 00:45:17 +1200

    complete

---
+++ Cats/Cats/CatInformation.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cats
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CatInformation : ContentPage
+	{
+		public CatInformation ()
+		{
+			InitializeComponent ();
+		}
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            Device.OpenUri(new Uri("http://www.spca.com/?page_id=4882&lang=en"));
+        }
+    }
+}
